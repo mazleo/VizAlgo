38a6b58f8a01be3fa0ccb47434c72e4755fb7a7f
Jonnelin Marzielli Leonardo <jonnelin.leonardo@gmail.com>
2020-01-02 16:08:31 -0500

    Add tooltips

---
+++ pathfinder/js/tooltips.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $('.algorithms-input-group label').tooltip({
+        animation: true,
+        trigger: 'hover'
+    });
+    $('.map-button').tooltip({
+        animation: true,
+        trigger: 'hover'
+    });
+    $('.app-button').tooltip({
+        animation: true,
+        trigger: 'hover'
+    });
+});
